db019840d9ed7e6893fa5b5fa50e361edad40204
Yue Cao <yue.cao.au@gmail.com>
2021-11-13 16:49:03 +1100

    set up google cloud
    set up google cloud

---
+++ install.R
@@ -0,0 +1,43 @@
+  
+
+# DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
+# use 'rake' to generate 'install.R'.
+
+if ("BiocManager" %in% rownames(installed.packages()))
+  remove.packages("BiocManager")
+
+install.packages("BiocManager", repos="https://cran.rstudio.com")
+#install.packages("devtools", repos="https://cran.rstudio.com")
+#devtools::install_github("Bioconductor/BiocManager")
+library(BiocManager)
+
+if(BiocManager::version() != "3.14"){
+  BiocManager::install(version="3.14",
+                       update=TRUE, ask=FALSE)
+}
+
+
+std_pkgs <- c(
+  "devtools", "edgeR",
+  "ggpubr", "MAST", 
+  "plyr", "Rtsne", 
+  "scales", "scater", "SingleCellExperiment",
+  "Cepo", "slingshot" , 
+  "scran", "tidyverse", 
+  "viridis", "monocle",
+  "ggthemes",  "ggridges", "reshape2" , "ggpubr", 
+  "DT", 
+  "DescTools")
+
+gh_pkgs <- c("MarioniLab/DropletUtils",
+             "SydneyBioX/scdney",
+             "SydneyBioX/scMerge",
+             "SydneyBioX/scClassify")
+
+builtins <- c(std_pkgs, gh_pkgs)
+
+for (builtin in builtins)
+  if (!suppressWarnings(require(builtin, character.only=TRUE)))
+    suppressWarnings(BiocManager::install(builtin,
+                                          version="3.14",
+                                          update=TRUE, ask=FALSE))
